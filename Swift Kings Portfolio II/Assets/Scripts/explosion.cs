e34c87aab623a94b7ba99db4283e45e4e6ff3675
TwinnedFool <ZDFrederick@student.fullsail.edu>
2023-06-07 07:35:46 -0600

    character projectile updated

--- Swift Kings Portfolio II/Assets/Scripts/explosion.cs
+++ Swift Kings Portfolio II/Assets/Scripts/explosion.cs
@@ -6,23 +6,22 @@ public class explosion : MonoBehaviour
 {
     [Range(0, 100)] [SerializeField] int dmg;
     [SerializeField] int pushbackAmount;
-    [SerializeField] Rigidbody rb;
     private void Start()
     {
         Destroy(gameObject,.75f);
     }
     private void OnTriggerEnter(Collider other)
     {
-        IDamage damageable = other.GetComponent<IDamage>();
-        if (damageable != null)
-        {
-            damageable.TakeDamage(dmg);
-        }
         IPhysics physicsable = other.GetComponent<IPhysics>();
         if (physicsable != null)
         {
             Vector3 dir = other.transform.position - transform.position;
             physicsable.TakePushBack(dir * pushbackAmount);
+        }
+        IDamage damageable = other.GetComponent<IDamage>();
+        if (damageable != null)
+        {
+            damageable.TakeDamage(dmg);
         }
     }
 }
