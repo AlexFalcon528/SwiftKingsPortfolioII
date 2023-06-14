f7e9784158dc7ede090972f9e7408ecb93687592
Ivoleonard <ibleonard@student.fullsail.edu>
2023-06-14 12:50:02 -0400

    sound downloads

--- Swift Kings Portfolio II/Assets/Scripts/Gravitymissile.cs
+++ Swift Kings Portfolio II/Assets/Scripts/Gravitymissile.cs
@@ -8,13 +8,16 @@ public class Gravitymissile : MonoBehaviour
     [Range(0, 100)] [SerializeField] int dmg;
     bool isGravitating;
     [SerializeField] Collider col;
+    [SerializeField] AudioSource aud;
+    [SerializeField] AudioClip[] soundeffect;
     // Start is called before the first frame update
     void Start()
     {
-
+        aud = gameManager.instance.player.GetComponent<AudioSource>();
+        
     }
     private void Update()
-    {
+    {   aud.PlayOneShot(soundeffect[Random.Range(0, soundeffect.Length)], audioManager.instance.audSFXVol);
         if(!isGravitating)
         {
             StartCoroutine(Gravitate());
